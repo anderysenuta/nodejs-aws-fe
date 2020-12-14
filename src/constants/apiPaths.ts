92c90f26e819761d6f98ef82b844dcc037a9916d
Andrei Sianiuta <anderysenuta@gmail.com>
2020-12-14 10:39:02 +0300

    TASK 8 change link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://hls6p04919.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://a80ff3jkpj.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://hls6p04919.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://anderysenuta-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
